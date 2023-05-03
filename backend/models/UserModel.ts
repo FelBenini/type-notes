6470490692db27542280094d466b107799519ed0
felbenini <felbenini@gmail.com>
2023-05-03 14:20:56 -0300

    Now usernames are unique dispite the uppercase/lowercase

--- backend/models/UserModel.ts
+++ backend/models/UserModel.ts
@@ -20,7 +20,7 @@ export interface IUserModel extends Model<IUserDocument> {
 }
 
 const userSchema = new mongoose.Schema<IUser>({
-    username: {type: String, required: true, unique: true, min: 3, max: 24},
+    username: {type: String, required: true, unique: true, min: 3, max: 24, lowercase: true},
     email: {type: String, required: true, unique: true, min: 5},
     displayName: {type: String, required: true},
     password: {type: String, required: true},
