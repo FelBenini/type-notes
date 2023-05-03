eb4c6e9a0c3147d7f08aab61f855d58d2e836e38
felbenini <felbenini@gmail.com>
2023-05-03 14:20:35 -0300

    Added sass to the frontend, added midlleware auth with jsonwebtokens on the backend

--- backend/index.ts
+++ backend/index.ts
@@ -1,17 +1,36 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express, Request, Response, NextFunction } from "express";
 import dotenv from 'dotenv'
 import jwt, { Secret } from "jsonwebtoken";
+import cors from 'cors'
 
 dotenv.config()
 const PORT = process.env.PORT;
 const privateKey: Secret = process.env.PRIVATE_KEY as Secret
 
 const app: Express = express()
+app.use(cors())
+
+const middlewareAuth = (req: Request, res: Response, next: NextFunction) => {
+    const token: string | undefined = req.headers.authorization
+    jwt.verify(token as string, privateKey, (err, decoded) => {
+        if (err) {
+            res.status(401).json({'message': 'Unauthorized'})
+        } else {
+            next()
+        }
+    })
+}
 
 app.get('/:name', (req: Request, res: Response) => {
     const {name} = req.params || 'Joe'
-    const token = jwt.sign({name: name}, privateKey)
+    const token = jwt.sign({name: name}, privateKey, {expiresIn: '60 days'})
     res.status(200).json({'Your key is': token})
+})
+
+app.get('/decode/:key', middlewareAuth, (req: Request, res: Response) => {
+    const {key} = req.params
+    const session = jwt.verify(key, privateKey)
+    res.status(200).json({session})
 })
 
 app.listen(PORT, () => {
