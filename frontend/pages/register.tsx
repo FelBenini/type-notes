adc6fd906150e83d07d871a9ebde2de184541a94
felbenini <felbenini@gmail.com>
2023-05-12 18:17:08 -0300

    Added a register form on the register page already fully working

--- frontend/pages/register.tsx
+++ frontend/pages/register.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { TextField, Button } from '@mui/material'
 import Link from 'next/link'
 import { Poppins } from 'next/font/google'
+import axios, { AxiosError } from 'axios'
 
 const poppins = Poppins({ subsets: ['latin'], weight: '600' })
 const poppinsParagraph = Poppins({ subsets: ['latin'], weight: '200' })
@@ -12,8 +13,19 @@ const RegisterPage = () => {
     const [username, setUsername] = useState('')
     const [errorMessage, setErrorMessage] = useState('')
 
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
+        try {
+            const res = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/register`, {
+                username: username,
+                email: email,
+                password: password
+            })
+        } catch (err: any) {
+            setErrorMessage(err.response.data.message)
+        }
+
+
     }
     return (
         <section id='login'>
@@ -22,7 +34,7 @@ const RegisterPage = () => {
                 <TextField value={email} onChange={e => setEmail(e.target.value)} required label='E-mail' sx={{ width: '95%' }} type='email' />
                 <TextField value={username} onChange={e => setUsername(e.target.value)} required label='Username' sx={{ width: '95%' }} type='text' />
                 <TextField value={password} onChange={e => setPassword(e.target.value)} required label='Password' sx={{ width: '95%' }} type='password' />
-                <p>{errorMessage}</p>
+                <p className={poppinsParagraph.className}>{errorMessage}</p>
                 <Button type='submit' variant='contained' sx={{ width: '95%', height: '42px', color: '#f6f7fa' }}>Create your account</Button>
                 <p className={poppinsParagraph.className}>Already have an account? <Link className={poppins.className} href='/login'>Login!</Link></p>
             </form>
